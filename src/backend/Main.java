b39ee517c716be92dda49d882948bd2481bebaa3
Bogdan Cosma <cosma_bogdancatalin@yahoo.com>
2020-11-04 20:42:49 +0200

    Start CelulaSexuata

--- src/backend/Main.java
+++ src/backend/Main.java
@@ -3,7 +3,13 @@ package backend;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
+import java.util.*;
+
+import backend.*;
+
 public class Main {
+
+	private CelulaSexuata[] cells = [];
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -11,7 +17,27 @@ public class Main {
 		Lock l = new ReentrantLock();
 		CelulaAsexuata c = new CelulaAsexuata(f, l);
 		Thread t = new Thread(c);
+
+		createListOfSexuateCells(f, l);
+
 		t.start();
 	}
 
+	private void createListOfSexuateCells(Food f, Lock l) {
+
+		CelulaSexuata cell1 = new CelulaSexuata(f, l);
+		CelulaSexuata cell2 = new CelulaSexuata(f, l);
+
+		this.cells.add(cell1);
+		this.cells.add(cell2);
+	}
+
+	private void verifyIfCellReadyInList(CelulaSexuata cell, Food f, Lock l) {
+
+		for(CelulaSexuata celula : this.cells) {
+			if(celula.getNewState() == cell.getNewState()) {
+				this.cells.add(new CelulaSexuata(f, l));
+			}
+		}
+	}
 }
